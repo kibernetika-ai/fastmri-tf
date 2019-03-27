ef7b792cfd56a7f94f6224173a669416f3510f92
Alexander Gunin <guninalexander@gmail.com>
2019-03-27 11:19:57 +0300

    cocofast

---
+++ coco.py
@@ -0,0 +1,207 @@
+import argparse
+import pathlib
+import random
+import numpy as np
+import tensorflow as tf
+import logging
+from cocofast.coco import CocoUnet
+from cocofast.coco import data_fn
+from models.data import null_dataset
+import os
+import json
+import time
+
+from kibernetika.kibernetika_utils import klclient
+
+
+def train(mode, checkpoint_dir, params):
+    logging.info("start build  model")
+
+    save_summary_steps = params['save_summary_steps']
+    save_checkpoints_secs = params['save_checkpoints_secs'] if params['save_checkpoints_steps'] is None else None
+    save_checkpoints_steps = params['save_checkpoints_steps']
+
+    conf = tf.estimator.RunConfig(
+        model_dir=checkpoint_dir,
+        save_summary_steps=save_summary_steps,
+        save_checkpoints_secs=save_checkpoints_secs,
+        save_checkpoints_steps=save_checkpoints_steps,
+        keep_checkpoint_max=params['keep_checkpoint_max'],
+        log_step_count_steps=params['log_step_count_steps'],
+    )
+    epoch_len, fn = data_fn(params, mode == 'train')
+    logging.info('Samples count: {}'.format(epoch_len))
+    params['epoch_len'] = epoch_len
+    net = CocoUnet(
+        params=params,
+        model_dir=checkpoint_dir,
+        config=conf,
+        warm_start_from=params['warm_start_from']
+
+    )
+    logging.info("Start %s mode type %s", mode, conf.task_type)
+    if mode == 'train' and conf.task_type != 'ps':
+        if conf.master != '':
+            train_fn = fn
+            train_spec = tf.estimator.TrainSpec(input_fn=train_fn)
+            eval_fn = null_dataset()
+            eval_spec = tf.estimator.EvalSpec(input_fn=eval_fn, steps=1, start_delay_secs=10, throttle_secs=10)
+            tf.estimator.train_and_evaluate(net, train_spec, eval_spec)
+        else:
+            net.train(input_fn=fn)
+    else:
+        train_fn = null_dataset()
+        train_spec = tf.estimator.TrainSpec(input_fn=train_fn)
+        eval_fn = fn
+        eval_spec = tf.estimator.EvalSpec(input_fn=eval_fn, steps=1, start_delay_secs=10, throttle_secs=10)
+        tf.estimator.train_and_evaluate(net, train_spec, eval_spec)
+
+
+def main(args):
+    params = {
+        'num_pools': args.num_pools,
+        'num_epochs': args.num_epochs,
+        'drop_prob': args.drop_prob,
+        'num_chans': args.num_chans,
+        'batch_size': args.batch_size,
+        'lr': args.lr,
+        'lr_step_size': args.lr_step_size,
+        'lr_gamma': args.lr_gamma,
+        'weight_decay': args.weight_decay,
+        'checkpoint': str(args.exp_dir),
+        'seed': args.seed,
+        'resolution': args.resolution,
+        'challenge': args.challenge,
+        'data_path': args.data_path,
+        'accelerations': args.accelerations,
+        'center_fractions': args.center_fractions,
+        'save_summary_steps': args.save_summary_steps,
+        'save_checkpoints_secs': args.save_checkpoints_secs,
+        'save_checkpoints_steps': args.save_checkpoints_steps,
+        'keep_checkpoint_max': args.keep_checkpoint_max,
+        'log_step_count_steps': args.log_step_count_steps,
+        'warm_start_from': args.warm_start_from,
+        'use_seed': False,
+        'resolution': args.resolution,
+        'data_set': args.data_set,
+    }
+    if not tf.gfile.Exists(args.exp_dir):
+        tf.gfile.MakeDirs(args.exp_dir)
+    if args.worker:
+        klclient.update_task_info({
+            'num-pools': args.num_pools,
+            'drop-prob': args.drop_prob,
+            'num-chans': args.num_chans,
+            'batch-size': args.batch_size,
+            'lr.lr': args.lr,
+            'lr.lr-step-size': args.lr_step_size,
+            'lr.lr-gamma': args.lr_gamma,
+            'weight-decay': args.weight_decay,
+            'checkpoint_path': str(args.checkpoint_dir),
+            'resolution': args.resolution
+        })
+        train('train', args.checkpoint_dir, params)
+    else:
+        cluster = {'chief': ['fake_worker1:2222'],
+                   'ps': ['fake_ps:2222'],
+                   'worker': ['fake_worker2:2222']}
+        os.environ['TF_CONFIG'] = json.dumps(
+            {
+                'cluster': cluster,
+                'task': {'type': 'evaluator', 'index': 0}
+            })
+        train('eval', args.exp_dir, params)
+
+
+def create_arg_parser():
+    conf_parser = argparse.ArgumentParser(
+        add_help=False
+    )
+    conf_parser.add_argument(
+        '--checkpoint_dir',
+        default=os.environ.get('TRAINING_DIR', 'training') + '/' + os.environ.get('BUILD_ID', '1'),
+        help='Directory to save checkpoints and logs')
+    args, remaining_argv = conf_parser.parse_known_args()
+    parser = argparse.ArgumentParser(
+        parents=[conf_parser],
+        description=__doc__,
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    checkpoint_dir = args.checkpoint_dir
+    logging.getLogger().setLevel('INFO')
+    tf.logging.set_verbosity(tf.logging.INFO)
+    parser.add_argument('--num-pools', type=int, default=4, help='Number of U-Net pooling layers')
+    parser.add_argument('--drop-prob', type=float, default=0.0, help='Dropout probability')
+    parser.add_argument('--num-chans', type=int, default=32, help='Number of U-Net channels')
+
+    parser.add_argument('--batch-size', default=16, type=int, help='Mini batch size')
+    parser.add_argument('--num-epochs', type=int, default=50, help='Number of training epochs')
+    parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+    parser.add_argument('--lr-step-size', type=int, default=40,
+                        help='Period of learning rate decay')
+    parser.add_argument('--lr-gamma', type=float, default=0.1,
+                        help='Multiplicative factor of learning rate decay')
+    parser.add_argument('--weight-decay', type=float, default=0.,
+                        help='Strength of weight decay regularization')
+
+    parser.add_argument('--resolution', default=320, type=int, help='Resolution of images')
+
+    parser.add_argument('--data_set', type=str, required=True,
+                        help='Path to the dataset')
+    parser.add_argument(
+        '--save_summary_steps',
+        type=int,
+        default=10,
+        help="Log summary every 'save_summary_steps' steps",
+    )
+    parser.add_argument(
+        '--save_checkpoints_secs',
+        type=int,
+        default=600,
+        help="Save checkpoints every 'save_checkpoints_secs' secs.",
+    )
+    parser.add_argument(
+        '--save_checkpoints_steps',
+        type=int,
+        default=100,
+        help="Save checkpoints every 'save_checkpoints_steps' steps",
+    )
+    parser.add_argument(
+        '--keep_checkpoint_max',
+        type=int,
+        default=2,
+        help='The maximum number of recent checkpoint files to keep.',
+    )
+    parser.add_argument(
+        '--log_step_count_steps',
+        type=int,
+        default=1,
+        help='The frequency, in number of global steps, that the global step/sec and the loss will be logged during training.',
+    )
+    parser.add_argument(
+        '--warm_start_from',
+        type=str,
+        default=None,
+        help='Warm start from',
+    )
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.set_defaults(worker=False)
+    group.set_defaults(evaluator=False)
+    group.set_defaults(test=False)
+    group.add_argument('--worker', dest='worker', action='store_true',
+                       help='Start in Worker(training) mode.')
+    group.add_argument('--evaluator', dest='evaluator', action='store_true',
+                       help='Start in evaluation mode')
+    group.add_argument('--test', dest='test', action='store_true',
+                       help='Test mode')
+    group.add_argument('--export', dest='export', action='store_true',
+                       help='Export model')
+    return parser
+
+
+if __name__ == '__main__':
+    logging.getLogger().setLevel('INFO')
+    args = create_arg_parser().parse_args()
+    random.seed(int(time.time()))
+    np.random.seed(int(time.time()))
+    main(args)
---
+++ cocofast/coco.py
@@ -0,0 +1,200 @@
+import tensorflow as tf
+import os
+from models.unet import unet
+import numpy as np
+import logging
+import json
+from kibernetika.rpt import MlBoardReporter
+from tensorflow.python.training import session_run_hook
+from tensorflow.python.training import training_util
+from tensorflow.python.training.session_run_hook import SessionRunArgs
+import math
+
+import cv2
+
+
+def data_fn(params, training):
+    data_set = params['data_set']
+    with open(data_set + '/coco2107/annotations/instances_train2017.json') as f:
+        data = json.load(f)
+    tmp = []
+    for a in data['annotations']:
+        if a['category_id'] == 1 and a['iscrowd'] == 0:
+            fname = data_set + '/coco2107/train2017/{:012d}.jpg'.format(a['image_id'])
+            if os.path.exists(fname):
+                tmp.append((a['segmentation'], fname))
+            else:
+                logging.info('Can\'t find image {:012d}.jpg'.format(a['image_id']))
+    data = tmp
+    resolution = params['resolution']
+
+    def _input_fn():
+        def _generator():
+            for i in data:
+                img = cv2.imread(data_set + '/coco2107/train2017/{:012d}.jpg'.format(i[1]), cv2.IMREAD_COLOR)[:, :,
+                      ::-1]
+                m = np.zeros((img.shape[0], img.shape[1], 1), np.float32)
+                for s in i[0]:
+                    p = np.array(s, np.int32)
+                    p = np.reshape(p, (1, int(p.shape[0] / 2), 2))
+                    m = cv2.fillPoly(m, p, color=(255, 255, 255))
+                img = cv2.resize(img, (resolution, resolution))
+                img = img.astype(np.float32) / 127.5 - 1
+                m = cv2.resize(m, (resolution, resolution)) / 127.5 - 1
+                yield (img, m)
+
+        ds = tf.data.Dataset.from_generator(_generator, (tf.float32, tf.float32),
+                                            (tf.TensorShape([resolution, resolution]),
+                                             tf.TensorShape([resolution, resolution])))
+        if training:
+            ds = ds.shuffle(params['batch_size'] * 2, reshuffle_each_iteration=True)
+        ds = ds.apply(tf.contrib.data.batch_and_drop_remainder(params['batch_size']))
+        if training:
+            ds = ds.repeat(params['num_epochs']).prefetch(params['batch_size'])
+        return ds
+
+    return len(data) // params['batch_size'], _input_fn
+
+
+def _unet_model_fn(features, labels, mode, params=None, config=None, model_dir=None):
+    features = tf.reshape(features, [params['batch_size'], params['resolution'], params['resolution'], 3])
+    training = (mode == tf.estimator.ModeKeys.TRAIN)
+    result = unet(features, 1, params['num_chans'], params['drop_prob'], params['num_pools'], training)
+    loss = None
+    train_op = None
+    hooks = []
+    export_outputs = None
+    eval_hooks = []
+    chief_hooks = []
+    metrics = {}
+    if mode != tf.estimator.ModeKeys.PREDICT:
+        learning_rate_var = tf.Variable(float(params['lr']), trainable=False, name='lr',
+                                        collections=[tf.GraphKeys.LOCAL_VARIABLES])
+        loss = tf.losses.absolute_difference(labels, result)
+        mse = tf.losses.mean_squared_error(labels, result)
+        nmse = tf.norm(labels - result) ** 2 / tf.norm(labels) ** 2
+
+        global_step = tf.train.get_or_create_global_step()
+        epoch = global_step // params['epoch_len']
+        if training:
+            tf.summary.scalar('lr', learning_rate_var)
+            tf.summary.scalar('mse', mse)
+            tf.summary.scalar('nmse', nmse)
+            board_hook = MlBoardReporter({
+                "_step": global_step,
+                "_epoch": epoch,
+                "_train_loss": loss,
+                '_train_lr': learning_rate_var,
+                '_train_mse': mse,
+                '_train_nmse': nmse}, every_steps=params['save_summary_steps'])
+            chief_hooks = [board_hook]
+            update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+            with tf.control_dependencies(update_ops):
+                opt = tf.train.RMSPropOptimizer(learning_rate_var, params['weight_decay'])
+                train_op = opt.minimize(loss, global_step=global_step)
+
+        tf.summary.image('Src', features, 3)
+        rimage = (result - tf.reduce_min(result))
+        rimage = rimage / tf.reduce_max(rimage)
+        tf.summary.image('Reconstruction', rimage, 3)
+        limage = (labels - tf.reduce_min(labels))
+        limage = limage / tf.reduce_max(limage)
+        tf.summary.image('Original', limage, 3)
+        hooks = [TrainingLearningRateHook(
+            params['epoch_len'],
+            learning_rate_var,
+            float(params['lr']),
+            int(params['lr_step_size']),
+            float(params['lr_gamma']))]
+        if not training:
+            metrics['mse'] = tf.metrics.mean(mse)
+            metrics['nmse'] = tf.metrics.mean(nmse)
+    else:
+        export_outputs = {
+            tf.saved_model.signature_constants.DEFAULT_SERVING_SIGNATURE_DEF_KEY: tf.estimator.export.PredictOutput(
+                result)}
+
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        eval_metric_ops=metrics,
+        predictions=result,
+        training_chief_hooks=chief_hooks,
+        loss=loss,
+        training_hooks=hooks,
+        export_outputs=export_outputs,
+        evaluation_hooks=eval_hooks,
+        train_op=train_op)
+
+
+class TrainingLearningRateHook(session_run_hook.SessionRunHook):
+    def __init__(self, epoch_len, learning_rate_var, initial_learning_rate, lr_step_size, lr_gamma):
+        self._learning_rate_var = learning_rate_var
+        self._lr_step_size = lr_step_size
+        self._lr_gamma = lr_gamma
+        self._epoch_len = epoch_len
+        self._prev_learning_rate = 0
+        self._initial_learning_rate = initial_learning_rate
+        self._epoch = -1
+
+    def begin(self):
+        self._global_step_tensor = training_util._get_or_create_global_step_read()  # pylint: disable=protected-access
+        if self._global_step_tensor is None:
+            raise RuntimeError("Global step should be created to use LearningRateHook.")
+        self._args = [self._global_step_tensor, self._learning_rate_var]
+        self._learning_rate_ph = tf.placeholder(tf.float32, name='learning_rate_ph')
+        self._learning_rate_op = self._learning_rate_var.assign(self._learning_rate_ph)
+
+    def after_create_session(self, session, coord):
+        session.run(self._learning_rate_op, {self._learning_rate_ph: self._initial_learning_rate})
+
+    def before_run(self, run_context):  # pylint: disable=unused-argument
+        return SessionRunArgs(self._args)
+
+    def after_run(self, run_context, run_values):
+        result = run_values.results
+        global_step = result[0]
+        learning_rate = result[1]
+        if learning_rate != self._prev_learning_rate:
+            logging.warning('Set Learning rate to {} at global step {}'.format(learning_rate, global_step))
+            self._prev_learning_rate = learning_rate
+        epoch = global_step // self._epoch_len
+
+        if self._epoch != epoch:
+            logging.info('Start epoch {}'.format(epoch))
+            self._epoch = epoch
+
+        lr_step = epoch // self._lr_step_size
+        if lr_step > 0:
+            desired_learning_rate = self._initial_learning_rate * math.pow(self._lr_gamma, lr_step)
+        else:
+            desired_learning_rate = self._initial_learning_rate
+
+        if self._prev_learning_rate != desired_learning_rate:
+            run_context.session.run(self._learning_rate_op, {self._learning_rate_ph: desired_learning_rate})
+
+
+class CocoUnet(tf.estimator.Estimator):
+    def __init__(
+            self,
+            params=None,
+            model_dir=None,
+            config=None,
+            warm_start_from=None
+    ):
+        def _model_fn(features, labels, mode, params, config):
+            return _unet_model_fn(
+                features=features,
+                labels=labels,
+                mode=mode,
+                params=params,
+                config=config,
+                model_dir=model_dir,
+            )
+
+        super(UNet, self).__init__(
+            model_fn=_model_fn,
+            model_dir=model_dir,
+            config=config,
+            params=params,
+            warm_start_from=warm_start_from
+        )
